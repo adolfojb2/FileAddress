062b8927dca9dd788aa097eee6b954a9e672ef50
Adolfo Betin <adolfojb2@gmail.com>
2022-07-24 10:44:45 -0500

    Se agrega un icono a la ventana

--- FileAddress.py
+++ FileAddress.py
@@ -2,8 +2,12 @@ from cgitb import text
 import tkinter
 from tkinter import filedialog
 from tkinter import font
+from tkinter import *
 raiz = tkinter.Tk()
 raiz.title("File Address")
+#se agrega el icono de la ventana
+img = PhotoImage(file='C:\\Users\\BetinBracamontes\\OneDrive - Universidad del Magdalena\\2022\\PYTHON\\FileAddress\\cadena.png')
+raiz.iconphoto(False,img)
 #abrir fichero
 def abrirfichero():
     rutafichero = filedialog.askopenfilename(title="Abrir Archivo")
